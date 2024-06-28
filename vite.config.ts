fd45d2d616cebb2917eb2d0b2df0166bc1e42cbd
Robby Wu <ta7382@gmail.com>
2024-06-29 02:52:01 +0800

    chore: Update file paths for favicon and script in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs'
-  }
+    outDir: 'docs',
+  },
+  base: '/react-google-sheets/docs/',
 })
