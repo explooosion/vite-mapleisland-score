0de4fd82aebd665dfe82ed294fd85fba6c0f7343
Robby Wu <ta7382@gmail.com>
2024-06-30 04:44:31 +0800

    chore: Update vite.config.ts to set the build output directory to 'docs'

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
+    build: {
+      outDir: 'docs',
+    },
     base: mode === 'production' ? '/vite-mapleisland-score/' : '/',
   }
 })
